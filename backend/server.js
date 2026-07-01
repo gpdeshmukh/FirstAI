4ea83245fa84fb9ac90243def2bc336ad0a6a207
Govind Deshmukh <gpdeshmukh8899@gmail.com>
2026-07-01 19:14:32 +0530

    Backend Setup with express and routes

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(express.json());
+
+const productRoutes = require("./routes/productRoutes");
+
+app.use("/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to FirstAI Backend");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
